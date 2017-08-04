7cf015819c26f3236db8f59476c7940fd1dfe547
galimru <galimovrf@gmail.com>
2017-08-04 18:44:43 +0400

    -- added spring boot application

---
+++ src/main/java/org/lizaalert/LizaalertApplication.java
@@ -0,0 +1,13 @@
+package org.lizaalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizaalertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LizaalertApplication.class);
+    }
+
+}

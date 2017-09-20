0e6120bec84b6abb0f2f95b1f661dfcccccaa947
galimru <galimovrf@gmail.com>
2017-09-20 18:41:54 +0400

    -- added class name for start command

--- src/main/resources/liquibase/db/20170817-1-insert-states.sql
+++ src/main/resources/liquibase/db/20170817-1-insert-states.sql
@@ -1,4 +1,4 @@
 --changeset galimru:insert-states
 
-INSERT INTO la_state (id, created_at, updated_at, parent_id, command)
-VALUES ('5ba38588-c161-4e8d-a7d1-a3755451f7db', now(), null, null, '/start');
+INSERT INTO la_state (id, created_at, updated_at, parent_id, command, class_name)
+VALUES ('5ba38588-c161-4e8d-a7d1-a3755451f7db', now(), null, null, '/start', 'org.lizaalert.commands.StartCommand');

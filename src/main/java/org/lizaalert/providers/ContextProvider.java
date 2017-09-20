c7362583c8f744a229557756ee8a1e2f5988d774
galimru <galimovrf@gmail.com>
2017-09-20 19:36:50 +0400

    -- fixed getting application context

--- src/main/java/org/lizaalert/providers/ContextProvider.java
+++ src/main/java/org/lizaalert/providers/ContextProvider.java
@@ -3,7 +3,9 @@ package org.lizaalert.providers;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ContextProvider implements ApplicationContextAware {
 
     private static ApplicationContext context;
